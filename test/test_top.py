f51857c1b8ee9f098328355838901dfe1f78c0bb
bigsdawg-2k <73724274+bigsdawg-2k@users.noreply.github.com>
2025-09-17 22:14:07 -0400

    A bit of clarity around test results. Reporting actual frequency for time-domain test instead of mean number of samples in a period...

--- test/test_top.py
+++ test/test_top.py
@@ -9,7 +9,7 @@ import utils.util_funcs as ut
 ])
 def test_create_square_wave_with_guassian(freq_Hz, duration_s, period_std_n):
     
-    print(f"\n\nRunning case f={freq_Hz}, d={duration_s}, and std_T_n={period_std_n}")
+    print(f"\n\nRunning case f={freq_Hz}Hz, d={duration_s}s, and std_T_n={period_std_n}samples")
             
     sample_rate_Hz = 192000
     period_std_s = period_std_n / sample_rate_Hz
@@ -21,17 +21,20 @@ def test_create_square_wave_with_guassian(freq_Hz, duration_s, period_std_n):
     # Perform time domain checks
     print(f"==Time domain checks==")
     mean, std, count = ut.analyze_transitions(wfm, 0.5, False)
+    freq_td_Hz = sample_rate_Hz / mean
     print(f"  Expected mean (in samples)={sample_rate_Hz/freq_Hz}; measured={mean}")
     print(f"  Expected std (in samples)={period_std_n}; measured={std}")
     print(f"  Expected count (in periods)={freq_Hz * duration_s}; measured={count}")
+    print(f"  Expected freq_td_Hz (Hz)={freq_Hz}; measured={freq_td_Hz}")
     assert mean == pytest.approx(sample_rate_Hz / freq_Hz, abs=0.01*sample_rate_Hz/freq_Hz)
     assert std == pytest.approx(period_std_n, abs=0.1*period_std_n)
-    assert count == pytest.approx(freq_Hz * duration_s, abs=2)
+    assert count == pytest.approx(freq_Hz * duration_s, abs=4*duration_s)
+    assert freq_td_Hz == pytest.approx(freq_Hz, abs=0.01*freq_Hz)
         
     # Perform frequency domain checks
     print(f"==Frequency domain checks==")
     freq_fft_Hz = ut.calculate_fundamental_frequency(wfm, sample_rate_Hz)
-    print(f"  Expected fft_freq (Hz)={freq_Hz}; measured={freq_fft_Hz}")
+    print(f"  Expected fft_freq (Hz)={freq_Hz}; measured={freq_fft_Hz}, resolution={1/duration_s}Hz")
     assert freq_fft_Hz == pytest.approx(freq_Hz, abs=0.01*freq_Hz)
     
     
